1b462ee45a96060e1279da2e3b02fb456ea06113
Doha Kaddaf <doha.kaddaf@gmail.com>
2024-07-14 18:28:03 +0200

    add products page main grid and filters

--- app.js
+++ app.js
@@ -18,10 +18,7 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-const capitalize = require("./utils/capitalize");
 const projectName = "sneakers-ecom-website-project";
-
-app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
@@ -29,6 +26,9 @@ app.use("/", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
+
+const productsRoutes = require("./routes/products.routes");
+app.use("/products", productsRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
