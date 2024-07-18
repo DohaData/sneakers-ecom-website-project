8c2641fb0e77558477ff641c664dea25cc1b63e3
Doha Kaddaf <doha.kaddaf@gmail.com>
2024-07-18 23:51:21 +0200

    finish cart related logic

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ app.use("/products", productsRoutes);
 const aboutRoutes = require("./routes/about.routes");
 app.use("/about", aboutRoutes);
 
+const cartRoutes = require("./routes/cart.routes");
+app.use("/cart", cartRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
